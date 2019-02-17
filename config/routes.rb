5e3cc02ae84b85748ab34bb2e97ae918f61b6f26
yuna-sowa <yuna@YunaMacBook-ea.local>
2019-02-17 17:38:19 +0900

    [WIP]チャット画面の実装
    # WHAT
    チャット画面のビューを実装する。
    
    # WHY
    サービスに必須であるため。
    ビューのみのため、機能は未実装。
    画面キャプチャあり。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'                        #ルートパスの指定
+  get  'messages'     => 'messages#index'      #メッセージ画面
 end
